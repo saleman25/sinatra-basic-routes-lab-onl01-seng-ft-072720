5086bb9c83158fdd4b71fc77358324832e472d4e
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-03 20:08:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+get '/name' do 
+    "My name is Samantha"
+  end
+
+  get '/hometown' do 
+    "My hometown is Los Angeles"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Always Be My Baby"
+  end
 end
